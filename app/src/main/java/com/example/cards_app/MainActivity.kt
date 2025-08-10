93ba9fc9739c630a12458974057666e630914562
erikg <erik.gregoric100@gmail.com>
2025-08-10 17:52:03 +0200

    made a dropdown menu, currently not yet functional

--- app/src/main/java/com/example/cards_app/MainActivity.kt
+++ app/src/main/java/com/example/cards_app/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,6 +14,8 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu // Example icon
 import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,7 +25,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cards_app.ui.theme.Cards_appTheme
@@ -70,14 +79,39 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyTopAppBar(title: String, modifier: Modifier = Modifier) {
+    var showDropdownMenu by remember { mutableStateOf(false) }
+    //val dropdownMenuItems: List<DropdownAction> = emptyList(): List<DropdownAction>
     TopAppBar(
         title = { Text(text = title) },
         modifier = modifier,
         navigationIcon = {
-            Icon(
-                imageVector = Icons.Filled.Menu,
-                contentDescription = "Menu"
-            )
+            Box {
+                IconButton(
+                    onClick = { showDropdownMenu = !showDropdownMenu }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Menu,
+                        contentDescription = "Main navigation menu"
+                    )
+                }
+                DropdownMenu(
+                    expanded = showDropdownMenu,
+                    onDismissRequest = { showDropdownMenu = false },
+                ) {
+                    val dropdownMenuItems = listOf("1", "2", "3")
+                    dropdownMenuItems.forEach { actionItem ->
+                        DropdownMenuItem(
+                            //text = { Text(actionItem.title) },
+                            text = { Text(actionItem) },
+                            onClick = {
+                                //actionItem.onClick()
+                                showDropdownMenu = false
+                            }
+                        )
+                    }
+                }
+            }
+
         },
         actions = {
             // Example action item:
