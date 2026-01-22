c680e2ae7e9507dda2c25abb6a43b2bd4c3f6af9
erik <erik.gregoric100@gmail.com>
2026-01-22 11:37:08 +0100

    Updated the `BASE_URL` in `RetrofitClient.kt` from the local emulator address to "https://enrico.host/".

--- app/src/main/java/com/example/cards_app/communication/RetrofitClient.kt
+++ app/src/main/java/com/example/cards_app/communication/RetrofitClient.kt
@@ -9,9 +9,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
-    // IMPORTANT: This is the local address for the Android emulator to connect to your PC.
-    // If your backend is somewhere else, change this URL.
-    private const val BASE_URL = "http://10.0.2.2:8080/"// TODO: Change this to your backend's URL
+    private const val BASE_URL = "https://enrico.host/"
     // http://10.0.2.2:8080/ is a special IP address that points to the Android emulator's localhost.
     // This client will be used to make the network requests.
     private val okHttpClient: OkHttpClient by lazy {
